072a163ee39ac965cbf08aa48bfaeb6293573631
phucdo2601 <ngocphucdo2601@gmail.com>
2022-01-05 20:31:01 +0700

    Basic-WidgetsBindingObserver and lifeCycle

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 // class MyApp extends StatelessWidget {
@@ -59,15 +58,49 @@ class MyApp extends StatefulWidget {
   }
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver{
+  //when a widget is created and running, there are 3 functions that we may concern:
+  //1. initState
+  //2. build() is triggered when we call setState(...)
+  //3. dispose() is called when state/widget object removed
+  //
   String _email = ""; // this is state
 
   final emailEditingController = TextEditingController();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance?.addObserver(this);
+    print('Run initState');
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    emailEditingController.dispose();
+    WidgetsBinding.instance?.removeObserver(this);
+    print('Run dispose');
+  }
+
+  //when I know "the app is in background/foreground mode"?
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    // TODO: implement didChangeAppLifecycleState
+    super.didChangeAppLifecycleState(state);
+    if(state == AppLifecycleState.paused) {
+      print('App is in Background mode');
+    }else if(state == AppLifecycleState.resumed) {
+      print('App is in Foreground mode');
+
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-
+    print('Run build()');
     // Now Add a state
     return MaterialApp(
       title: "This is a StatefulWidget",
