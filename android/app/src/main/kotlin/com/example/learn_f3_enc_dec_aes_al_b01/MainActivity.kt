ccde0e440bdc385c4f77693aeb1d820a18a94096
phucdo2601 <ngocphucdo2601@gmail.com>
2022-07-30 21:55:01 +0700

    First learn basic encrypt and decrypt string by AES Algo b01

---
+++ android/app/src/main/kotlin/com/example/learn_f3_enc_dec_aes_al_b01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.learn_f3_enc_dec_aes_al_b01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:encrypt/encrypt.dart' as encrypt;
+import 'package:learn_f3_enc_dec_aes_al_b01/my_encryption.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  /**
+   * Some useful variable
+   */
+  TextEditingController tec = TextEditingController();
+  var encryptedText, plainText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Encryption/Decryption"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextField(
+                controller: tec,
+              ),
+            ),
+            Column(
+              children: [
+                Text(
+                  "Plain Text",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.blue.shade400,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(plainText == null ? "" : plainText),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Text(
+                  "ENCRYPTED TEXT",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.blue.shade400,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(encryptedText == null
+                      ? ""
+                      : encryptedText is encrypt.Encrypted
+                          ? encryptedText.base64
+                          : encryptedText),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    plainText = tec.text;
+                    setState(() {
+                      encryptedText =
+                          MyEncryptionDecryption.encryptAES(plainText);
+                    });
+                  },
+                  child: Text("Encrypt"),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      encryptedText = MyEncryptionDecryption.decrytAES(encryptedText);
+                    });
+                  },
+                  child: Text("Decrypt"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
