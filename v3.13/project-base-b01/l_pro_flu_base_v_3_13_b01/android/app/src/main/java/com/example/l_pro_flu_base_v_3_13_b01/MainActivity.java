b2a5a4dcb867e2f6f165dc1f3d34ffdf8eba97fb
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-08-19 09:26:25 +0700

    [learn-flutter-pro-list] 01-Re-structure and Initial the project b01

---
+++ v3.13/project-base-b01/l_pro_flu_base_v_3_13_b01/android/app/src/main/java/com/example/l_pro_flu_base_v_3_13_b01/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.l_pro_flu_base_v_3_13_b01;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ v3.13/project-base-b01/l_pro_flu_base_v_3_13_b01/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Material(
+        child: Column(
+          children: [
+            Text("Hello World"),
+            Text("Login to continue"),
+          ],
+        ),
+      ),
+    )
+  );
+}
+
+
+
